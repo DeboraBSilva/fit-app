cd7300411f0422feb4c8452a837a735277c192cd
DeboraBSilva <deb.barussi@gmail.com>
2022-03-28 15:09:30 -0300

    Fix Exercise and Routine tests

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,5 @@ class Exercise < ApplicationRecord
   has_and_belongs_to_many :routines
 
   validates :description, presence: true
-  validates_inclusion_of :intensity, in: 0..10, presence: true
+  validates_inclusion_of :intensity, in: 0..10
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Exercise, type: :model do
   subject { described_class.new(description: 'Exercise 1', intensity: 0) }
 
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:intensity) }
-  it { is_expected.to validate_inclusion_of(:intensity).in(0..10) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_inclusion_of(:intensity).in_range(0..10) }
 end
