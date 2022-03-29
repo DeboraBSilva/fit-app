43e5716babd73490b8c570f46242a1149c49ebbc
DeboraBSilva <deb.barussi@gmail.com>
2022-03-29 09:54:37 -0300

    Create User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :encrypted_password, presence: true
+  validates :reset_password_token, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # subject { described_class.new(email: 'john@email.com', cpf: '99999999999', born_on: '1998-01-22') }
+  subject { described_class.new(email: 'john@email.com', encrypted_password: 'password') }
 
-  # it { is_expected.to validate_presence_of(:name) }
-  # it { is_expected.to validate_presence_of(:cpf) }
-  # it { is_expected.to validate_uniqueness_of(:cpf).case_insensitive }
-  # it { is_expected.to validate_length_of(:cpf).is_equal_to(11) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_presence_of(:encrypted_password) }
+  it { is_expected.to validate_uniqueness_of(:reset_password_token) }
 end
