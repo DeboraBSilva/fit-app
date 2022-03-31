cd89ed76ee60833f929509aef34289764cd6a52c
DeboraBSilva <deb.barussi@gmail.com>
2022-03-31 15:42:09 -0300

    Add dependent destroy to Exercise and Routine models

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,5 +1,5 @@
 class Routine < ApplicationRecord
-  has_many :exercise_routines
+  has_many :exercise_routines, dependent: :destroy
   has_many :exercises, through: :exercise_routines
 
   validates :name, presence: true
